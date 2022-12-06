4f63dd6306fd666d00941fa0f758604ff579f941
abid luay raihan taufik <spaskeren30@gmail.com>
2022-12-06 11:32:19 +0700

    FILE

--- Praktikum6py.py
+++ Praktikum6py.py
@@ -1,14 +1,13 @@
-# Basproglabpy6
-```sh
 dataMahasiswa = {}
 
 print("=" * 65)
-print("|\tPROGRAM INPUT NILAI MAHASISWA MENGGUNAKAN FUNGSI\t|")
+print(" \tPROGRAM INPUT NILAI MAHASISWA MENGGUNAKAN FUNGSI\t ")
 print("=" * 65)
 
+
 def tambah():
-        nama = str(input("Masukan Nama : "))
-        nim = int(input("Masukan Nim   : "))
+        nama = str(input("Masukan Nama \t: "))
+        nim = int(input("Masukan Nim   \t: "))
         tugas = int(input("Masukan Nilai Tugas : "))
         uts = int(input("Masukan Nilai UTS     : "))
         uas = int(input("Masukan Nilai UAS     : "))
@@ -17,13 +16,13 @@ def tambah():
         print("\nDATA BERHASIL DI TAMBAHKAN!")
 def tampilkan():
         print("=" * 69)
-        print("|" + "\t" * 3 + "DAFTAR NILAI MAHASISWA" + "\t" * 3 +
-                  "    |")
+        print(" " + "\t" * 3 + "DAFTAR NILAI MAHASISWA" + "\t" * 3 +
+                  "     ")
         print("=" * 69)
         print("| NO |   \tNAMA\t   |\tNIM \t| TUGAS | UTS | UAS | AKHIR |")
         print("=" * 69)
+        no = 0
         for tampil in dataMahasiswa.items():
-            no = 0
             no += 1
             print("| {6:2} |\t {0:15}   | {1:9} \t| {2:5} | {3:3} | {4:3} | {5:5} |".format(tampil[0], tampil[1][0], tampil[1][1], tampil[1][2], tampil[1][3],"%.2f" % float(tampil[1][4]), no))
             print("=" * 69)
@@ -33,7 +32,7 @@ def hapus(nama):
  
 def ubah(nama):
         if nama in dataMahasiswa.keys():
-            nim = int(input("Masukan Nim  : "))
+            nim = int(input("Masukan Nim  \t: "))
             tugas = int(input("Masukan Nilai Tugas : "))
             uts = int(input("Masukan Nilai UTS     : "))
             uas = int(input("Masukan Nilai UAS     : "))
@@ -41,11 +40,11 @@ def ubah(nama):
             dataMahasiswa[nama] = nim, tugas, uts, uas, akhir
             print("\nDATA BERHASIL DI UBAH!")
         else:
-            print("\DATA TIDAK DI TEMUKAN!")
+            print("\nDATA TIDAK DI TEMUKAN!")
 
 while True:
     data = input(
-        "\n 1 - Tambah Data,\t 2 - Tampilkan Data,\t 3 - Hapus Data,\t 4 - Ubah Data, 5 - Keluar \n : "
+        "\n [1] Tambah Data, [2] Tampilkan Data, [3] Hapus Data\n [4] Ubah Data, [5] Keluar : "
     )
     if (data.lower() == '1'):
         tambah()
@@ -64,30 +63,15 @@ while True:
             tampilkan()
         else:
             print("=" * 69)
-            print("|" + "\t" * 3 + "DAFTAR NILAI MAHASISWA" + "\t" * 3 +
-                  "    |")
+            print(" " + "\t" * 3 + "DAFTAR NILAI MAHASISWA" + "\t" * 3 +
+                  "     ")
             print("=" * 69)
             print("| NO |   \tNAMA\t   |\tNIM \t| TUGAS | UTS | UAS | AKHIR |")
             print("=" * 69)
-            print("|    " + "\t" * 3 + "TIDAK ADA DATA!" + "\t" * 4 + "    |")
+            print("     " + "\t" * 3 + "TIDAK ADA DATA!" + "\t" * 4 + "     ")
             print("=" * 69)
     elif (data.lower() == '5'):
         print("\nTERIMA KASIH! \n")
         exit()
     else:
         print("PILIHAN MENU TIDAK ADA!")
-
-```
-### Penjelasan
-1. Deklarasi dictionary dengan nama dataMahasiswa untuk menampung semua data/element.
-2. gunakan fungsi def tambah() di isi dengan inputan nama, nim, tugas, uts, uas dan perhitungan nilai akhir untuk dan di masukan ke dictonary dataMahasiswa.
-3. gunakan fungsi def tampilkan() di isi dengan cetak isi dari dictonary.
-4.  gunakan fungsi def hapus(nama) di isi dengan syntax delet untuk menghapus element nama pada dictonary dataMahasiswa.
-5. gunakan fungsi def ubah(nama) di isi dengan inputan nama dan mengubah isi element pada nama tersebut.
-6. gunakan while True untuk menlooping/mengulang statment.
-7. gunakan statment if, elif, else, di dalam while True dan panggil fungsi dari masing masing fungsi, contoh:
-### <b>Flowchart</b>
-![img](ss/flowchart.jpeg)
-
-### <b>Output program</b>
-![img](ss/Screenshot%20(45).png)
